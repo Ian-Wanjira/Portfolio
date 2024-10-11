1116b1a3d17ed679304110a5cc5c073a689c39c6
Ian-Wanjira <ianwanjira4@gmail.com>
2024-10-10 21:41:32 -0500

    style: add darkmode, container class and colors

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,16 +1,22 @@
-import type { Config } from "tailwindcss";
+import type { Config } from 'tailwindcss';
 
 const config: Config = {
+  darkMode: ['class'],
   content: [
-    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    './pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './components/**/*.{js,ts,jsx,tsx,mdx}',
+    './app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
+    container: {
+      center: true,
+      padding: '2rem',
+    },
     extend: {
       colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
+        primary: '#00122C',
+        secondary: '#0FEE82',
+        darkPrimary: '#00122C',
       },
     },
   },
