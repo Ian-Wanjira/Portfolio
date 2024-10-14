7d18baf017448578f46a1e9f5e77422529999505
Ian-Wanjira <ianwanjira4@gmail.com>
2024-10-14 04:13:49 -0500

    feat: create responsive hero banner section

--- app/page.tsx
+++ app/page.tsx
@@ -1,101 +1,81 @@
-import Image from "next/image";
+import Image from 'next/image';
+import Link from 'next/link';
 
-export default function Home() {
+import Header from '@/components/Header';
+import { Button } from '@/components/ui/button';
+const Home = () => {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="https://nextjs.org/icons/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
-
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
+    <main className="bg-primary">
+      <Header />
+      <div className=" flex md:flex-col items-center justify-center md:py-20 fixed top-24 md:top-1/4 md:left-0 md:pl-12">
+        <div className="hidden md:flex md:flex-col gap-20 md:gap-6 md:mb-6 w-full justify-center items-center  md:items-start">
+          <Link
+            href={'https://github.com/Ian-Wanjira'}
             target="_blank"
-            rel="noopener noreferrer"
+            className="hover:fill-secondary"
           >
             <Image
-              className="dark:invert"
-              src="https://nextjs.org/icons/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
+              src="/assets/icons/mingcute_github-line.svg"
+              alt="github"
               height={20}
+              width={20}
             />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
+          </Link>
+          <Link
+            href={'https://www.linkedin.com/in/ian-wanjira-96659b21a/'}
             target="_blank"
-            rel="noopener noreferrer"
           >
-            Read our docs
-          </a>
+            <Image
+              src="/assets/icons/lucide_linkedin.svg"
+              alt="github"
+              height={20}
+              width={20}
+            />
+          </Link>
+          <Link href={'https://x.com/techsavant__'} target="_blank">
+            <Image
+              src="/assets/icons/mingcute_twitter-line.svg"
+              alt="github"
+              height={20}
+              width={20}
+            />
+          </Link>
         </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
+        <div className="vl"></div>
+      </div>
+      <div className="hidden md:fixed md:flex md:flex-col items-center w-8 right-0 top-72 pr-12">
+        <p className="rotate-90 text-white-400 mb-20 ">ianwanjira4@gmail.com</p>
+        <div className="vl"></div>
+      </div>
+      <div className="container">
+        <section className="h-screen flex items-center justify-center md:gap-10 md:pt-20">
+          <div className="md:text-center flex flex-col gap-8 md:gap-0 md:items-center">
+            <h3 className="font-firaMono text-sm  md:text-base leading-4 text-secondary md:pb-6">
+              Hi my name is
+            </h3>
+            <h1 className="text-4xl font-semibold md:text-7xl md:pb-10 text-white-200 md:leading-tight">
+              Ian Wanjira. <br></br>
+              <span className="text-white-300">I build Great Software.</span>
+            </h1>
+            <p className="text-base max-w-sm md:max-w-2xl md:text-xl md:pb-10 text-white-400">
+              I’m a FullStack engineer specializing in building (and
+              occasionally designing) exceptional digital experiences.
+              Currently, I’m focused on building accessible, human-centered
+              products.
+            </p>
+            <Button
+              variant="outline"
+              className="w-60 text-secondary border-secondary font-firaMono"
+            >
+              Hire Me Today
+            </Button>
+          </div>
+        </section>
+
+        <section id="about">This is the about section</section>
+      </div>
+    </main>
   );
-}
+};
+
+export default Home;
