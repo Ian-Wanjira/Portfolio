be9cc1978bf7a7d342ceec71274eac0e3bbad1bc
Ian-Wanjira <ianwanjira4@gmail.com>
2024-10-14 04:25:38 -0500

    feat: add navigation link constants

---
+++ constants/index.ts
@@ -0,0 +1,22 @@
+export const navLinks = [
+  {
+    number: 1,
+    route: '#about',
+    label: 'About',
+  },
+  {
+    number: 2,
+    route: '#experience',
+    label: 'Experience',
+  },
+  {
+    number: 3,
+    route: '#projects',
+    label: 'Projects',
+  },
+  {
+    number: 4,
+    route: '#contact',
+    label: 'Contact',
+  },
+];
