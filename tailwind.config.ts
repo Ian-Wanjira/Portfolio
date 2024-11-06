6e2db0e4c39132a573fc2dab45603068409b1c38
Ian-Wanjira <ianwanjira4@gmail.com>
2024-11-06 01:13:07 -0600

    update tailwind configuration with new colors, font-sizes, and background images

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,10 +8,10 @@ const config: Config = {
     './app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
-    container: {
-      center: true,
-      padding: '3rem',
-    },
+    // container: {
+    //   center: true,
+    //   padding: '3rem',
+    // },
     extend: {
       colors: {
         primary: '#00122C',
@@ -19,11 +19,21 @@ const config: Config = {
         darkPrimary: '#00122C',
         blue: {
           100: '#00275F',
+          200: '#85c7f2',
+          300: '#00275F',
+          400: '#003C92',
+          500: '#001734',
         },
         white: {
           200: '#D4DCFF',
           300: '#8E9AAB',
           400: '#838B8A',
+        },
+        sand: {
+          100: '#f3d3bd',
+        },
+        green: {
+          100: '#3DF39B',
         },
       },
       rotate: {
@@ -32,6 +42,12 @@ const config: Config = {
       fontFamily: {
         inter: ['var(--font-inter)', 'sans-serif'],
         firaMono: ['var(--font-fira-mono)', 'monospace'],
+      },
+      fontSize: {
+        '4xl': '2.5rem',
+      },
+      backgroundImage: {
+        health: "url('/assets/images/onboarding.jpg')",
       },
     },
   },
