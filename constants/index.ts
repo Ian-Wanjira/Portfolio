5e1acc34b141a3ebf04b5a4adb70c50254bb1edd
Ian-Wanjira <ianwanjira4@gmail.com>
2024-11-06 01:10:52 -0600

    feat: add technologies array and Projects array with patient-management details

--- constants/index.ts
+++ constants/index.ts
@@ -20,3 +20,43 @@ export const navLinks = [
     label: 'Contact',
   },
 ];
+
+export const Technologies = [
+  'Next.js',
+  'React',
+  'TypeScript',
+  'Angular',
+  'React-Native',
+  'Django',
+  'Flask',
+  'Docker',
+  'Sentry',
+];
+
+export const Projects = [
+  {
+    title: 'Patient Management System',
+    description:
+      'A healthcare patient management system that allows patients to easily register, book, and manage their appointments with doctors, featuring administrative tools for scheduling, confirming, and canceling appointments, along with SMS notifications, all built using Next.js and DjangoRestFramework.',
+    imageUrl: '/assets/images/onboarding.jpg',
+    projectUrl: 'https://patient-management-frontend-five.vercel.app/',
+    technologies: [
+      'Next.js',
+      'TypeScript',
+      'TanStackQuery',
+      'Shadcn/ui',
+      'TailwindCSS',
+      'Sentry',
+      'DjangoRestFramework',
+      'PostgreSQL',
+      'Redis',
+      'Celery',
+      'Docker',
+      'Twilio',
+    ],
+    githubLinks: [
+      'https://github.com/Ian-Wanjira/patient-management-frontend',
+      'https://github.com/Ian-Wanjira/patient-management-api',
+    ],
+  },
+];
