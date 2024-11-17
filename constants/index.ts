55e3ca504e629c383e7f3c5fa2558e821660d7d0
Ian-Wanjira <ianwanjira4@gmail.com>
2024-11-17 13:13:24 -0600

    feat: Add E-Commerce Website project and update Patient Management System image
    - Updated the image URL for the Patient Management System project to '/assets/images/onboarding1.jpg'.
    - Added a new project titled 'E-Commerce Website' to the projects list with details including title, description, image URL, project URL, technologies used, and GitHub link.

--- constants/index.ts
+++ constants/index.ts
@@ -38,7 +38,7 @@ export const Projects = [
     title: 'Patient Management System',
     description:
       'A healthcare patient management system that allows patients to easily register, book, and manage their appointments with doctors, featuring administrative tools for scheduling, confirming, and canceling appointments, along with SMS notifications, all built using Next.js and DjangoRestFramework.',
-    imageUrl: '/assets/images/onboarding.jpg',
+    imageUrl: '/assets/images/onboarding1.jpg',
     projectUrl: 'https://patient-management-frontend-five.vercel.app/',
     technologies: [
       'Next.js',
@@ -58,5 +58,21 @@ export const Projects = [
       'https://github.com/Ian-Wanjira/patient-management-frontend',
       'https://github.com/Ian-Wanjira/patient-management-api',
     ],
+  },
+  {
+    title: 'E-Commerce Website',
+    description:
+      'Tech Haven is an innovative E-Commerce platform built to cater to tech enthusiasts and Apple device lovers. This project showcases a user-friendly website where customers can browse, purchase, and explore a wide range of Apple products and other cutting-edge tech gadgets.',
+    imageUrl: '/assets/images/e-commerce.png',
+    projectUrl: 'https://e-commerce-tech.payloadcms.app/',
+    technologies: [
+      'Next.js',
+      'TypeScript',
+      'SCSS',
+      'PayloadCMS',
+      'Stripe',
+      'Docker',
+    ],
+    githubLinks: ['https://github.com/Ian-Wanjira/e-commerce'],
   },
 ];
